3ed5d5e25765372aa04d1bc9cff97c15cfb42f1f
Luiz Fernando de Cristo Moloni <luiz.moloni@gmail.com>
2021-04-09 10:31:51 -0300

    Criação da Página Comparecimento e Abstenção

--- src/pages/Eleitorado.js
+++ src/pages/Eleitorado.js
@@ -37,6 +37,20 @@ function Eleitorado() {
   const [load, setLoad] = useState(false);
   const container = useRef(null);
 
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets:[
+      {
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [65, 59, 80, 81, 56, 55, 40],
+      }
+    ]
+  }
+
   useEffect(() => {
     if (load === true) {
       lottie.loadAnimation({
@@ -58,6 +72,10 @@ function Eleitorado() {
           <span className='title'> Perfil do Eleitorado </span>
         </div>
 
+        <Col>
+         <data/>        
+        </Col>
+
         <Row>
           <Col lg='3'>
             <EleitoradoFilter />
@@ -73,7 +91,7 @@ function Eleitorado() {
             </>
             ) : (
               <>
-                {/* -----------------------------------------------    */}
+                {/* -----------------------------------------------    
                 {filtroAplicado ?
                   (<>
                     <span>filtro aplicado :)</span>
