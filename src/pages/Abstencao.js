55dedeeb8af7d719e3a9dc66efeadaba2fb35aa8
JOAOM <joaomarcos8922@gmail.com>
2021-05-28 16:46:57 -0300

    Gráficos relevantes inseridos nos botões e separados por categoria

--- src/pages/Abstencao.js
+++ src/pages/Abstencao.js
@@ -56,6 +56,11 @@ export default function Abstencao() {
   const [qtEleitores, setQtEleitores] = useState({});
 
   const [DadosRelevantesButton, setDadosRelevantesButton] = useState(false);
+  const [DadosRelevantesEscButton, setDadosRelevantesEscButton] =
+    useState(false);
+  const [DadosRelevantesECButton, setDadosRelevantesECButton] = useState(false);
+  const [DadosRelevantesTotalButton, setDadosRelevantesTotalButton] =
+    useState(false);
 
   useEffect(() => {
     if (loading) {
@@ -168,18 +173,6 @@ export default function Abstencao() {
       <div>
         <div>
           <h5>
-            <b> Maiores Abstenções </b>
-          </h5>
-          <Bar data={MaxAbsten} options={options} />
-        </div>
-        <div>
-          <h5>
-            <b> Menores Abstenções </b>
-          </h5>
-          <Bar data={MinAbsten} options={options} />
-        </div>
-        <div>
-          <h5>
             <b> Maiores Abstenções entre jovens </b>
           </h5>
           <Bar data={Maxjovens} options={options} />
@@ -214,6 +207,13 @@ export default function Abstencao() {
           </h5>
           <Bar data={MaxIdosos} options={options} />
         </div>
+      </div>
+    );
+  };
+
+  const ChartRelevantesEsc = () => {
+    return (
+      <div>
         <div>
           <h5>
             <b> Maiores Abstenções entre analfabetos</b>
@@ -250,6 +250,12 @@ export default function Abstencao() {
           </h5>
           <Bar data={MinSuperiorCompleto} options={options} />
         </div>
+      </div>
+    );
+  };
+  const ChartRelevantesEC = () => {
+    return (
+      <div>
         <div>
           <h5>
             <b> Menores Abstenções entre casados </b>
@@ -273,6 +279,24 @@ export default function Abstencao() {
             <b> Maiores Abstenções entre solteiros </b>
           </h5>
           <Bar data={MaxSolteiros} options={options} />
+        </div>
+      </div>
+    );
+  };
+  const ChartRelevantesTotal = () => {
+    return (
+      <div>
+        <div>
+          <h5>
+            <b> Maiores Abstenções </b>
+          </h5>
+          <Bar data={MaxAbsten} options={options} />
+        </div>
+        <div>
+          <h5>
+            <b> Menores Abstenções </b>
+          </h5>
+          <Bar data={MinAbsten} options={options} />
         </div>
       </div>
     );
@@ -305,6 +329,41 @@ export default function Abstencao() {
               {filtroAplicado ? (
                 <Col lg="7">
                   <Chart />
+                  {!DadosRelevantesTotalButton ? (
+                    <Row className="d-flex justify-content-start mr-3">
+                      <Button
+                        onClick={() =>
+                          setDadosRelevantesTotalButton(
+                            !DadosRelevantesTotalButton
+                          )
+                        }
+                        style={{
+                          backgroundColor: "#214bb5",
+                        }}
+                      >
+                        Mostrar Dados Relevantes - Totais
+                      </Button>
+                    </Row>
+                  ) : (
+                    <div>
+                      <Row className="d-flex justify-content-start mr-3">
+                        <Button
+                          onClick={() =>
+                            setDadosRelevantesTotalButton(
+                              !DadosRelevantesTotalButton
+                            )
+                          }
+                          style={{
+                            backgroundColor: "#214bb5",
+                          }}
+                        >
+                          Esconder Dados Relevantes - Totais
+                        </Button>
+                      </Row>
+                      <h3 className="renda-title">Dados Relevantes - Totais</h3>
+                      <ChartRelevantesTotal className="charts" />
+                    </div>
+                  )}
                   {!DadosRelevantesButton ? (
                     <Row className="d-flex justify-content-start mr-3">
                       <Button
@@ -315,7 +374,7 @@ export default function Abstencao() {
                           backgroundColor: "#214bb5",
                         }}
                       >
-                        Mostrar Dados Relevantes
+                        Mostrar Dados Relevantes - Faixa Etária
                       </Button>
                     </Row>
                   ) : (
@@ -329,11 +388,81 @@ export default function Abstencao() {
                             backgroundColor: "#214bb5",
                           }}
                         >
-                          Esconder Dados Relevantes
+                          Esconder Dados Relevantes - Faixa Etária
                         </Button>
                       </Row>
-                      <h3 className="renda-title">Dados Relevantes</h3>
+                      <h3 className="renda-title">
+                        Dados Relevantes - Faixa Etária
+                      </h3>
                       <ChartRelevantes className="charts" />
+                    </div>
+                  )}
+                  {!DadosRelevantesEscButton ? (
+                    <Row className="d-flex justify-content-start mr-3">
+                      <Button
+                        onClick={() =>
+                          setDadosRelevantesEscButton(!DadosRelevantesEscButton)
+                        }
+                        style={{
+                          backgroundColor: "#214bb5",
+                        }}
+                      >
+                        Mostrar Dados Relevantes - Escolaridade
+                      </Button>
+                    </Row>
+                  ) : (
+                    <div>
+                      <Row className="d-flex justify-content-start mr-3">
+                        <Button
+                          onClick={() =>
+                            setDadosRelevantesEscButton(
+                              !DadosRelevantesEscButton
+                            )
+                          }
+                          style={{
+                            backgroundColor: "#214bb5",
+                          }}
+                        >
+                          Esconder Dados Relevantes - Escolaridade
+                        </Button>
+                      </Row>
+                      <h3 className="renda-title">
+                        Dados Relevantes - Escolaridade
+                      </h3>
+                      <ChartRelevantesEsc className="charts" />
+                    </div>
+                  )}
+                  {!DadosRelevantesECButton ? (
+                    <Row className="d-flex justify-content-start mr-3">
+                      <Button
+                        onClick={() =>
+                          setDadosRelevantesECButton(!DadosRelevantesECButton)
+                        }
+                        style={{
+                          backgroundColor: "#214bb5",
+                        }}
+                      >
+                        Mostrar Dados Relevantes - Estado Civil
+                      </Button>
+                    </Row>
+                  ) : (
+                    <div>
+                      <Row className="d-flex justify-content-start mr-3">
+                        <Button
+                          onClick={() =>
+                            setDadosRelevantesECButton(!DadosRelevantesECButton)
+                          }
+                          style={{
+                            backgroundColor: "#214bb5",
+                          }}
+                        >
+                          Esconder Dados Relevantes - Estado Civil
+                        </Button>
+                      </Row>
+                      <h3 className="renda-title">
+                        Dados Relevantes - Estado Civil
+                      </h3>
+                      <ChartRelevantesEC className="charts" />
                     </div>
                   )}
                 </Col>
